340bc6a839973ad0c9a5450df9db49ffb1c561c1
john kaiser <52598487+johnkaizer@users.noreply.github.com>
2023-01-19 20:44:35 +0300

    nav drawer

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,17 @@
 
 b
 2app/src/main/java/com/project/codetuts/SignIn.java,e\7\e7504bdae8416ec57bf484266e465afe8ebfa564
+P
+ app/src/main/AndroidManifest.xml,8\c\8c55c3ccc257e5907959013f99656e4c8ec3903e
+r
+Bapp/src/main/java/com/project/codetuts/Adapters/CourseAdapter.java,e\2\e2086995f2fea5db68124105d007afed60370c20
+v
+Fapp/src/main/java/com/project/codetuts/Adapters/CoursesAdapterAll.java,5\6\560e173e169847c1307102cda787be21e61cc8b9
+n
+>app/src/main/java/com/project/codetuts/Model/CoursesModel.java,2\9\29e6aa767b1740903ab632e29b89730987f68835
+h
+8app/src/main/java/com/project/codetuts/MainActivity.java,8\7\873fd2b14f7522b3eeb8c8c4653c795be44510d5
+m
+=app/src/main/java/com/project/codetuts/SetttingsActivity.java,4\5\45465e8f8c51c61abf32cc20075e749063bf207a
+p
+@app/src/main/java/com/project/codetuts/ui/home/HomeFragment.java,7\7\7778f8229fecee85263fbbe6089794c197d5dd94
--- app/src/main/java/com/project/codetuts/ui/favorite/FavoriteFragment.java
+++ app/src/main/java/com/project/codetuts/ui/favorite/FavoriteFragment.java
@@ -1,4 +1,4 @@
-package com.project.codetuts.ui.slideshow;
+package com.project.codetuts.ui.favorite;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,22 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.project.codetuts.databinding.FragmentSlideshowBinding;
+import com.project.codetuts.databinding.FragmentGalleryBinding;
 
-public class SlideshowFragment extends Fragment {
+public class FavoriteFragment extends Fragment {
 
-    private FragmentSlideshowBinding binding;
+    private FragmentGalleryBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        SlideshowViewModel slideshowViewModel =
-                new ViewModelProvider(this).get(SlideshowViewModel.class);
+        GalleryViewModel galleryViewModel =
+                new ViewModelProvider(this).get(GalleryViewModel.class);
 
-        binding = FragmentSlideshowBinding.inflate(inflater, container, false);
+        binding = FragmentGalleryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textSlideshow;
-        slideshowViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextView textView = binding.textGallery;
+        galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
